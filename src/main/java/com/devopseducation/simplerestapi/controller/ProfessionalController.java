9c55fbfa32eac91ca9a2e84ef42fa21181608a25
lhpfranco <lhpfranco@hotmail.com>
2022-05-15 23:54:50 -0300

    Adding post and delete endpoints

--- src/main/java/com/devopseducation/simplerestapi/controller/ProfessionalController.java
+++ src/main/java/com/devopseducation/simplerestapi/controller/ProfessionalController.java
@@ -3,8 +3,15 @@ package com.devopseducation.simplerestapi.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devopseducation.simplerestapi.model.Professional;
@@ -13,14 +20,10 @@ import com.devopseducation.simplerestapi.model.Professional;
 @RequestMapping("/professionals")
 public class ProfessionalController {
 	
+	private List<Professional> professionals;
 	
-	@GetMapping("/list")
-	List<Professional> all() {
-	  return getProfessionals();
-	}
-	
-	private List<Professional> getProfessionals() {
-		List<Professional> professionals = new ArrayList<Professional>();
+	ProfessionalController(){
+		professionals = new ArrayList<Professional>();
 		
 		Professional one = new Professional();
 		one.setId("A0001");
@@ -34,8 +37,41 @@ public class ProfessionalController {
 		
 		professionals.add(one);
 		professionals.add(two);
+	}
+	
+	
+	@GetMapping("/list")
+	List<Professional> all() {
+	  return this.professionals;
+	}
+	
+	
+	@PostMapping("/add")
+	public ResponseEntity<HttpStatus> postController(
+	  @RequestBody Professional professional) {
 		
-		return professionals;
+		Professional newProfessional = new Professional();
+		newProfessional.setId(professional.getId());
+		newProfessional.setName(professional.getName());
+		newProfessional.setRole(professional.getRole());
+		professionals.add(newProfessional);
+		
+		return ResponseEntity.ok(HttpStatus.OK);
+	}
+	
+	
+	@DeleteMapping("/delete/{id}")
+	public @ResponseBody ResponseEntity<String> delete(@PathVariable(value = "id") String id) {
+		
+		boolean professionalRemoved = professionals.removeIf(e -> e.getId().equals(id));
+		
+		if(professionalRemoved) {
+			return new ResponseEntity<String>("Success: Professional deleted", HttpStatus.OK);
+		
+		}else {
+			return new ResponseEntity<String>("Failure: Id not found", HttpStatus.BAD_REQUEST);
+		}
+		
 	}
 
 }
